1e303fb494f782f13f57841fd84c404ccb53452f
Sruthi Mora <sruthimora5@gmail.com>
2024-09-02 19:20:21 +0530

    Add templating using ejs
    Change list to reflect as Weekday or Weekend List based on current day

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+app.set("view engine","ejs");
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname + "/index.html");
+    let today = new Date();
+    let day = today.getDay();
+    let typeOfDay;
+    if(day===0 || day===6)  
+        typeOfDay = "Weekend";
+    else    typeOfDay = "Weekday";
+    console.log(typeOfDay);
+    res.render("index",{typeOfDay: typeOfDay});
 });
 
 app.post('/', function(req,res){
-    const newItem = req.body.newItem;
-    res.send("<h2>" + newItem + "</h2>");
+    
 });
 
 app.listen(3000, function(){
