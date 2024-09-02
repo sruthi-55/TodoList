47d956450530adf76566196fddb8f82e2a4f6c48
Sruthi Mora <sruthimora5@gmail.com>
2024-09-02 20:01:35 +0530

    Implement add items to todo list

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+let items = [];
 const app = express();
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+    var options = {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+    };
+    var today = new Date();
 
-app.get('/', function(req,res){
-    let today = new Date();
-    let day = today.getDay();
-    let typeOfDay;
-    if(day===0 || day===6)  
-        typeOfDay = "Weekend";
-    else    typeOfDay = "Weekday";
-    console.log(typeOfDay);
-    res.render("index",{typeOfDay: typeOfDay});
+    let curDay = today.toLocaleString("en-us", options);
+
+    res.render("index", { day: curDay, items: items });
 });
 
-app.post('/', function(req,res){
-    
+app.post("/", function (req, res) {
+    items.push(req.body.newItem);
+    res.redirect('/');
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server listening at port 3000");
 });
